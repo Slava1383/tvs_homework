00550ef2b456c6d0ec91d0387c6ca944f88cfdc0
Viacheslav Tarabarin <slava1383@gmail.com>
2024-02-25 14:19:29 +0200

    hometask5

---
+++ hometask5/src/main/java/task1/Main.java
@@ -0,0 +1,9 @@
+package task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Vaska", 3, "Siamese");
+        cat1.info();
+        cat1.newName("Petka");
+    }
+}
